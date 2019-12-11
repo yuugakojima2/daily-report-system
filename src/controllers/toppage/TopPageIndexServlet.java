c32c0081b66b0128d2c50202ae27e3f058f08f40
sasakama <sasakama3193@gmail.com>
2019-12-11 17:50:54 +0900

    Add login / logout link

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -15,22 +15,29 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/index.html")
 public class TopPageIndexServlet extends HttpServlet {
-        private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TopPageIndexServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TopPageIndexServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-        /**
-         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-         */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
-                rd.forward(request, response);
-        }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (request.getSession().getAttribute("flush") != null) {
+			request.setAttribute("flush", request.getSession().getAttribute("flush"));
+			request.getSession().removeAttribute("flush");
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/topPage/index.jsp");
+		rd.forward(request, response);
+	}
 
 }
