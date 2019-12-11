3130215834296c879cf893e4f093f9d88fd61ec5
sasakama <sasakama3193@gmail.com>
2019-12-12 04:57:52 +0900

    Add Report new

--- src/controllers/login/LoginServlet.java
+++ src/controllers/login/LoginServlet.java
@@ -34,7 +34,6 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	// ログイン画面を表示
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setAttribute("_token", request.getSession().getId());
@@ -52,7 +51,6 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	// ログイン処理を実行
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 認証結果を格納する変数
--- src/models/Employee.java
+++ src/models/Employee.java
@@ -12,10 +12,10 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Table(name = "employees")
-@NamedQueries({ @NamedQuery(name = "getAllEmployees", query = "select e from Employee as e order by e.id desc"),
-		@NamedQuery(name = "getEmployeesCount", query = "select count(e) from Employee as e"),
-		@NamedQuery(name = "checkRegisteredCode", query = "select count(e) from Employee as e where e.code = :code"),
-		@NamedQuery(name = "checkLoginCodeAndPassword", query = "select e from Employee as e where e.delete_flag=0 and e.password=:pass") })
+@NamedQueries({ @NamedQuery(name = "getAllEmployees", query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"),
+		@NamedQuery(name = "getEmployeesCount", query = "SELECT COUNT(e) FROM Employee AS e"),
+		@NamedQuery(name = "checkRegisteredCode", query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"),
+		@NamedQuery(name = "checkLoginCodeAndPassword", query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass") })
 @Entity
 public class Employee {
 	@Id
@@ -107,5 +107,4 @@ public class Employee {
 	public void setDelete_flag(Integer delete_flag) {
 		this.delete_flag = delete_flag;
 	}
-
 }
